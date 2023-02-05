94e983c78dbc7e621157e431efc81d40983c9d48
Somaro <106792137+SomarGuy@users.noreply.github.com>
2023-02-04 22:01:25 -0500

    fix POST issue when registering & add error message for inputs

--- routes/auth.js
+++ routes/auth.js
@@ -13,4 +13,33 @@ router.get('/users/dashboard', (req, res) => {
   res.render("dashboard", { user: "Diogo"});
 })
 
+router.post('/users/register', (req, res) => {
+  let { name, email, password, password2 } = req.body;
+
+  console.log({
+    name,
+    email,
+    password,
+    password2
+  });
+
+  let errors = [];
+
+  if (!name || !email || !password || !password2) {
+    errors.push({ message: "Please fill in all fields"});
+  }
+
+  if (password.length < 4){
+    errors.push({ message: "Password should be at least 4 cahracters"});
+  }
+
+  if(password != password2) {
+    errors.push({ message: "Passwords do not match"});
+  }
+
+  if(errors.length > 0) {
+    res.render("register", { errors });
+  }
+})
+
 module.exports = router;
