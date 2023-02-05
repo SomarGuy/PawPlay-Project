e29ebbc2944dd9ab0d4f5f2cae4969473b914596
jrivera604 <joshrivera91@hotmail.com>
2023-02-05 02:18:02 +0000

    change last_name and first_name into name

--- db/tables.sql
+++ db/tables.sql
@@ -1,8 +1,7 @@
 CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
-  last_name VARCHAR(255) NOT NULL,
-  first_name VARCHAR(255) NOT NULL,
-  password VARCHAR(32) NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL,
   email VARCHAR(255) NOT NULL,
   is_admin BOOLEAN
 );
